d577c99c09016ac11204fd6f7c8bf1a3c8ea7f2b
zilong.zhou <zzl81cn@163.com>
2016-08-17 17:56:26 +0800

    1.Add CMD.

---
+++ src/CMD/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhouzilong on 2016/8/17.
+ */
+define(function(require, exports, module){
+    var util = require('util');
+    util.log("I'm main module, i allready is ok'");
+})
---
+++ src/CMD/js/util.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhouzilong on 2016/8/17.
+ */
+define(function(require, exports, module){
+    var _prefix = "I want speak";
+    module.exports = {
+        log: function(msg){
+            console.log(_prefix + msg)}
+    }
+})
