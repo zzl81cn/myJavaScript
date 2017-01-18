de99e0359a161709d39ca2c372fbe06877d81649
zzl81cn <zzl81cn@163.com>
2017-01-18 18:06:52 +0800

    补充示例注释

--- src/CMD/js/main.js
+++ src/CMD/js/main.js
@@ -2,6 +2,7 @@
  * Created by zhouzilong on 2016/8/17.
  */
 define(function(require, exports, module){
+	// 引入util模块
     var util = require('util');
     util.log("I'm main module, i allready is ok'");
 })
