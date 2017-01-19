1f7b77f00c445dfdce5cb6ae26e7e30125fe71d9
zzl81cn <zzl81cn@163.com>
2017-01-19 14:27:44 +0800

    Modify CMD examples utils add Math.min() method.

--- src/CMD/js/main.js
+++ src/CMD/js/main.js
@@ -5,4 +5,6 @@ define(function(require, exports, module){
 	// 引入util模块
     var util = require('util');
     util.log("I'm main module, i allready is ok'");
+    util.mathMin(12, 20);
+    util.mathMin(12, 20);
 })
--- src/CMD/js/util.js
+++ src/CMD/js/util.js
@@ -5,7 +5,17 @@
 define(function(require, exports, module){
     var _prefix = "I want speak";
     module.exports = {
+    	/**
+		 * Console.log info
+		 * */
         log: function(msg){
-            console.log(_prefix + msg)}
+            console.log(_prefix + msg);
+        },
+		/**
+		 * Caculate min value and DOM write
+		 * */
+		mathMin: function (x, y) {
+			document.write(Math.min(x, y),'<br/>');
+		}
     }
 })
