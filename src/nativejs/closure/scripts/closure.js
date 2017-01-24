1ed4b5c58fc7b2ec3353b4d2c2cc6bebde9ce17f
zzl81cn <zzl81cn@163.com>
2017-01-24 13:43:53 +0800

    对闭包示例进行了页面调用。

--- src/nativejs/closure/scripts/closure.js
+++ src/nativejs/closure/scripts/closure.js
@@ -2,18 +2,20 @@
  * Created by zhouzilong on 2016/8/18.
  */
 
-/*这个例子是一个比较简单的闭包，解释如下：
-
-(1)定义了一个普通函数A
-(2)在A中定义了普通函数B
-(3)在A中返回B（确切的讲，在A中返回B的引用）
-(4)执行A(),把A的返回结果赋值给变量 c
-(5)执行 c()
-当一个内部函数被其外部函数之外的变量引用时，就形成了一个闭包。*/
+/**
+ * 这个例子是一个比较简单的闭包，解释如下：
+ * (1)定义了一个普通函数A
+ * (2)在A中定义了普通函数B
+ * (3)在A中返回B（确切的讲，在A中返回B的引用）
+ * (4)执行A(),把A的返回结果赋值给变量 c
+ * (5)执行 c()
+ * 当一个内部函数被其外部函数之外的变量引用时，就形成了一个闭包。
+ * */
 
 function simpleClosure(){
     function innerFunc(){
         console.log("Hello zzl81cn");
+		document.write('Hello zzl81cn!');
     }
     return innerFunc;
 }
@@ -26,6 +28,7 @@ function recycleClosure(){
     function innerRecycle(){
         count++;
         console.log(count);
+        document.write(count+'<br/>');
     }
     return innerRecycle;
 }
