7ce9298ee347385815d4dbbdc3913a81bdc6ea66
周紫龙 <zzl81cn@163.com>
2018-01-12 17:06:34 +0800

    Modify seajs CMD

--- src/CMD/js/main.js
+++ src/CMD/js/main.js
@@ -3,8 +3,17 @@
  */
 define(function(require, exports, module){
 	// 引入util模块
-    var util = require('util');
+    /*var util = require('util');
     util.log("I'm main module, i allready is ok'");
     util.mathMin(12, 20);
-    util.mathMin(12, 20);
+    util.mathMin(12, 20);*/
+
+    var $ = require('jquery');
+    // var jquery = require('jquery');
+    $(function () {
+      var a = $('.js-example-basic-single').val();
+      console.log(a)
+    })
+    // require('select2')($);
+
 })
