ecded06e1b5aad2eaaf797d3982665a2c06ee431
zzl81cn <zzl81cn@163.com>
2018-08-31 10:51:28 +0800

    增加用第一个参数对比第二个参数的长度（位数）和浮点数的精确乘法计算（含小数点后精度参数）

--- src/raw-js/Math/scripts/math.js
+++ src/raw-js/Math/scripts/math.js
@@ -17,6 +17,26 @@ define('math',function () {
 			var tmpElement = document.createElement('p');
 			tmpElement.appendChild(tmpMinValueNode);
 			document.getElementById('result').appendChild(tmpElement);
-		}
+		},
+		// 用第一个参数对比第二个参数的长度（位数）
+		isLength: function(n, length) {
+			return n.length == length;
+		},
+		// 浮点数的精确乘法计算（含小数点后精度参数）
+		accMul: function accMul(arg1, arg2, fixed) {
+			var m = 0,
+				s1 = arg1.toString(),
+				s2 = arg2.toString();
+			// 因为是乘法计算，需要将小数点后面的位数累计追加给m，方便利用Math.pow用x的y次幂的方式得出除数
+			try {
+				m += s1.split(".")[1].length
+			} catch(e) {}
+			try {
+				m += s2.split(".")[1].length
+			} catch(e) {}
+			var result = Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+			return result.toFixed(fixed);
+		},
+		
 	}
 });
