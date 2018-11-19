68453b19521524924f7d9aa7ad76cac169ab2521
zzl81cn <zzl81cn@163.com>
2018-11-19 15:27:35 +0800

    Modify myTable plugins. Add ajax method.

--- src/jQuery/custom-plugins/table/src/js/index.js
+++ src/jQuery/custom-plugins/table/src/js/index.js
@@ -3,6 +3,7 @@
     this.elem = elem; /* 1 */
     this.$elem = $(elem); /* 2 */
     this.options = options; /* 3 */
+    // this.url = options.url || ''; /* URL */
   };
 
   MyTable.prototype = {
@@ -11,61 +12,51 @@
     },
     init: function() {
       this.config = $.extend({}, this.defaults, this.options);
-      this.renderTable();
+      // this.renderTable();
+      this.fetchData();
     },
-    renderTable: function() {
+    renderTable: function(data) {
       // var $target = $(target);
       this.$elem.addClass('table');
-      if(this.config.bordered) {
+      if (this.config.bordered) {
+        this.$elem.css('border', '1px solid #ccc');
+      } if (this.config.bordered) {
         this.$elem.addClass('table-border')
       } else {}
       var $tableBody = $(
         '<thead>'
         + '<tr><th>xx</th><th>xx</th></tr></thead>'
-        + '<tbody><tr><td>1</td><td>5</td></tr>'
+        + '<tbody><tr><td>1</td><td>7</td></tr>'
         + '</tbody>').appendTo(this.elem);
       // console.log('tableBody is: ', $tableBody);
       return this.$elem;
     },
+    fetchData: function() {
+      var that = this;
+      console.log('fetch data.')
+      $.ajax({
+        type: 'GET',
+        url: this.config.url,
+        dataType: 'JSON',
+        success: function(data) {
+          console.log('data is: ', data.data.length);
+          if(data.data.length) {
+            that.renderTable(data);
+          }
+        },
+        error: function() {
+          console.log('Table ajax is error.')
+        }
+      })
+    },
   }
-
-  function renderTable(target, options) {
-    var $target = $(target);
-    $target.addClass('table');
-    if(options.bordered) {
-      $target.addClass('table-border')
-    } else {}
-    /* $target.html(function () {
-      var tableStr = '';
-      tableStr += '<thead><tr><th>xx</th><th>xx</th></tr></thead>';
-      tableStr += '<tbody><tr><td>1</td><td>2</td></tr></tbody>'
-      return tableStr;
-    }) */
-    var $tableBody = $('<thead><tr><th>xx</th><th>xx</th></tr></thead><tbody><tr><td>1</td><td>2</td></tr></tbody>').appendTo(target);
-    // console.log('tableBody is: ', $tableBody);
-    return target;
-  };
-
   
-  /* $.fn.myTable = function(options) {
-    var opts = $.extend({}, $.fn.myTable.defaults, options);
-    return this.each(function() {
-      var target = $(this);
-      renderTable(target, options);
-    })
-  }; */
   MyTable.defaults = MyTable.prototype.defaults;
 
   $.fn.myTable = function(options) {
-    // var opts = $.extend({}, $.fn.myTable.defaults, options);
     return this.each(function() {
       new MyTable(this, options).init();
-      // var target = $(this);
-      // renderTable(target, options);
     })
   };
 
-  $.fn.myTable.defaults = {
-    bordered: true
-  }
 })(jQuery);
