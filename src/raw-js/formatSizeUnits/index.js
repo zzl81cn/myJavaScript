3c1a886c559c08d260537c272451f7df07b3fd87
zzl81cn <zzl81cn@163.com>
2025-02-24 16:17:55 +0800

    add formatSizeUnits

---
+++ src/raw-js/formatSizeUnits/index.js
@@ -0,0 +1,11 @@
+function formatSizeUnits(kb) {
+  let units = ['KB', 'MB', 'GB', 'TB', 'PB'];
+  let unitIndex = 0;
+
+  while (kb >= 1024 && unitIndex < units.length - 1) {
+    kb /= 1024;
+    unitIndex++;
+  }
+
+  return `${kb.toFixed(2)} ${units[unitIndex]}`;
+}
