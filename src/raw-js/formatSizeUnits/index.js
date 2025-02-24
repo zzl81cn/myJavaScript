50cda5244144f4723df9fdb5cecfc40c095d9f01
zzl81cn <zzl81cn@163.com>
2025-02-24 16:22:48 +0800

    update

--- src/raw-js/formatSizeUnits/index.js
+++ src/raw-js/formatSizeUnits/index.js
@@ -1,3 +1,9 @@
+/**
+ * @description 只写后台管理的前端要怎么提升自己 https://www.jianshu.com/p/5e8fc96f5b7b 2025????(20250224)
+ * @param {*} kb 
+ * @returns 
+ */
+
 function formatSizeUnits(kb) {
   let units = ['KB', 'MB', 'GB', 'TB', 'PB'];
   let unitIndex = 0;
